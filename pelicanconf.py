3ba2ab2130fc56cdda0ece9b88a06b5ea656d75e
Sang Han <jjangsangy@gmail.com>
2014-05-05 00:47:06 -0700

    init

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,18 +5,19 @@ from __future__ import unicode_literals
 AUTHOR = 'Sang Han'
 SITENAME = 'Sang Han'
 SITEURL = 'http://sanghan.me'
+EMAIL_ADDR = 'jjangsangy@gmail.com'
 
 TIMEZONE = 'America/Los Angeles'
-
 DEFAULT_LANG = u'en'
 DATE_FORMATS = {'en': '%a, %d %b %Y'}
 STATIC_PATHS = ['images']
-EMAIL_ADDR = 'jjangsangy@gmail.com'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
+
+THEME = './pelican-bootstrap3'
 
 # Blogroll
 LINKS =  (('Photography', 'http://sanghanphotography.com'),
