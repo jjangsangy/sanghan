11b96ca213422607d3168bb1d640b7adc448636a
Sang Han <jjangsangy@gmail.com>
2014-05-29 00:56:32 -0800

    Update documentation

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,1 +1,17 @@
-var angular.module('blogApp', ['ui.bootstrap']);
+'use strict';
+
+app.controller('DropdownCtrl', function($scope) {
+    $scope.status = {
+        isopen: false;
+    };
+
+    $scope.toggled = function(open) {
+        console.log('Dropdown is now: ', open);
+    };
+
+    $scope.toggleDropdown = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+});
