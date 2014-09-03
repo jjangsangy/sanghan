dfd02b7629060dd3f29c1155bbc294f3025f236d
Sang Han <jjangsangy@gmail.com>
2014-09-03 12:37:12 -0800

    Update documentation

--- static/js/app.js
+++
@@ -1,1 +0,0 @@
-var app = angular.module('blogApp', ['ui.bootstrap', 'controllers.js']);
---
+++ static/js/site.js
@@ -0,0 +1,3 @@
+// Add slideDown animation to dropdown
+$(".dropdown").on('show.bs.dropdown', function(e) { $(this).find('.dropdown-menu').first().stop(true, true).slideDown(); });
+$('.dropdown').on('hide.bs.dropdown', function(e) { $(this).find('.dropdown-menu').first().stop(true, true).slideUp(); });
