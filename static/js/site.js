786c7cf400a23c4f7a46efcec1624f5ac9f9b934
Sang Han <jjangsangy@gmail.com>
2014-09-03 12:40:02 -0800

    Update documentation

--- static/js/site.js
+++
@@ -1,3 +0,0 @@
-// Add slideDown animation to dropdown
-$(".dropdown").on('show.bs.dropdown', function(e) { $(this).find('.dropdown-menu').first().stop(true, true).slideDown(); });
-$('.dropdown').on('hide.bs.dropdown', function(e) { $(this).find('.dropdown-menu').first().stop(true, true).slideUp(); });
