bb75142dac913165f540b7e6890020b2e5716a93
Sang Han <jjangsangy@gmail.com>
2014-09-08 13:16:50 -0800

    Update documentation

--- static/js/site.js
+++ static/js/site.js
@@ -1,4 +1,11 @@
 $(window).load(function() {
+    // var stixStyle = [".mi", ".mo", ".mtext"];
+    // stixStyle.map(function(stix) {
+    //     $(stix+"[style*='STIXGeneral']").each(function() {
+    //         var avenir = $(this).attr("style").replace("STIXGeneral", "AvenirNext");
+    //         return $(this).attr("style", avenir);
+    //     });
+    // });
     $(".mo[style*='STIXGeneral']").each(function() {
         var avenir = $(this).attr("style").replace("STIXGeneral", "AvenirNext");
         return $(this).attr("style", avenir);
