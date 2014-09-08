e8208eeca91af48e2f2569e9358272eb468dd1c8
Sang Han <jjangsangy@gmail.com>
2014-09-08 10:21:16 -0800

    Update documentation

--- static/js/site.js
+++ static/js/site.js
@@ -1,2 +1,10 @@
-$(document).ready(function{
+$(window).bind("load", function() {
+    $(".mo[style*='STIXGeneral']").each(function() {
+        var avenir = $(this).attr("style").replace("STIXGeneral", "AvenirNext");
+        return $(this).attr("style", avenir);
+    });
+    $(".mi[style*='STIXGeneral']").each(function() {
+        var avenir = $(this).attr("style").replace("STIXGeneral", "AvenirNext");
+        return $(this).attr("style", avenir);
+    });
 });
