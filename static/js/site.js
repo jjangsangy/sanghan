e68df29c4a208666a3ae5445a8805b0da2860f9f
Sang Han <jjangsangy@gmail.com>
2014-09-08 20:04:27 -0800

    Update documentation

--- static/js/site.js
+++ static/js/site.js
@@ -6,16 +6,16 @@ $(window).load(function() {
     //         return $(this).attr("style", avenir);
     //     });
     // });
-    $(".mo[style*='STIXGeneral']").each(function() {
-        var avenir = $(this).attr("style").replace("STIXGeneral", "AvenirNext");
-        return $(this).attr("style", avenir);
-    });
-    $(".mi[style*='STIXGeneral']").each(function() {
-        var avenir = $(this).attr("style").replace("STIXGeneral", "AvenirNext");
-        return $(this).attr("style", avenir);
-    });
-    $(".mtext[style*='STIXGeneral']").each(function() {
-        var avenir = $(this).attr("style").replace("STIXGeneral", "AvenirNext");
-        return $(this).attr("style", avenir);
-    });
+    // $(".mo[style*='STIXGeneral']").each(function() {
+    //     var avenir = $(this).attr("style").replace("STIXGeneral", "AvenirNext");
+    //     return $(this).attr("style", avenir);
+    // });
+    // $(".mi[style*='STIXGeneral']").each(function() {
+    //     var avenir = $(this).attr("style").replace("STIXGeneral", "AvenirNext");
+    //     return $(this).attr("style", avenir);
+    // });
+    // $(".mtext[style*='STIXGeneral']").each(function() {
+    //     var avenir = $(this).attr("style").replace("STIXGeneral", "AvenirNext");
+    //     return $(this).attr("style", avenir);
+    // });
 });
